f0f986627a46456f008e74154c40a08dc912da0a
viktoravramenko <viktor.avramenkl@yandex.ru>
2026-09-23 21:29:36 +0300

    wip: start pagination in TransactionRepository

--- src/main/java/com/financetracker/finance_tracker/repository/TransactionRepository.java
+++ src/main/java/com/financetracker/finance_tracker/repository/TransactionRepository.java
@@ -2,20 +2,25 @@ package com.financetracker.finance_tracker.repository;
 
 import com.financetracker.finance_tracker.entity.Transaction;
 import com.financetracker.finance_tracker.entity.TransactionType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.Date;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByType(TransactionType Type);
+    Page<Transaction> findByType(TransactionType Type, Pageable pageable);
 
-    List<Transaction> findByCategoryId(Long categoryId);
+    Page<Transaction> findByCategoryId(Long categoryId, Pageable pageable);
+
+    Page<Transaction> findByDate(Date date, Pageable pageable);
+
+    Page<Transaction> findByAmount(BigDecimal amount, Pageable pageable);
 
     @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.type = :paramName")
     public BigDecimal sumAmountByType(@Param("paramName")TransactionType type);
-
 }
